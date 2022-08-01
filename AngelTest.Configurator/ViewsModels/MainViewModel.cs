2c98982f1b8502a516305ec26765e4962bbe3455
imreallykind <dimaelfimov228@gmail.com>
2022-08-02 01:28:15 +0300

    continuing

--- AngelTest.Configurator/ViewsModels/MainViewModel.cs
+++ AngelTest.Configurator/ViewsModels/MainViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AngelTest.Configurator.ViewsModels
 {
@@ -13,5 +15,10 @@ namespace AngelTest.Configurator.ViewsModels
         private Page UsersAdd;
         private Page UsersPg;
         private Page MainWindow;
+
+        public MainViewModel()
+        {
+            MainWindow = new Pages.MainWindow();
+        }
     }
 }
