a95acddbe36f0acd5e5d4cbb54d946a555ad1c7f
imreallykind <dimaelfimov228@gmail.com>
2022-08-02 01:15:46 +0300

    switched to mvvm(xd)

---
+++ AngelTest.Configurator/Views/UsersPg.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace AngelTest.Configurator.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersPg.xaml
+    /// </summary
+    public partial class UsersPg : MetroWindow
+    {
+        public UsersPg()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AngelTest.Configurator/ViewsModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AngelTest.Configurator.ViewsModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private Page UsersAdd;
+        private Page UsersPg;
+        private Page MainWindow;
+    }
+}
